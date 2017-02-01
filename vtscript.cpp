416bfd540428040f225fee54f71673796dad5af2
Nyle Rodgers <nyle.rodgers@gmail.com>
2017-02-01 13:56:34 -0500

    Create vtscript.cpp file

---
+++ vtscript.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>
+#include "tokenize.hpp"
+#include "expression.hpp"
+#include "environment.hpp"
+#include "interpreter.hpp"
+
+int main(int argc, char * argv[]) {
+    return EXIT_SUCCESS;
+}
