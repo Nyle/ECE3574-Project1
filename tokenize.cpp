f801de7d523a1bd4695deb43db9e3ec2dba54411
Nyle Rodgers <nyle.rodgers@gmail.com>
2017-02-02 10:29:24 -0500

    add function to tokenize input stream

--- tokenize.cpp
+++ tokenize.cpp
@@ -1,1 +1,26 @@
 #include "tokenize.hpp"
+#include <ctype.h>
+
+std::vector<std::string> tokenize(std::istream & in) {
+    std::vector<std::string> tokens; // List of tokens found
+    
+    std::string symbol = "";    // Hold multi-character symbols
+    for (char tmp = in.get(); !in.eof(); tmp = in.get()) {
+        if ((tmp == '(' || tmp == ')' || isspace(tmp)) && symbol.length() > 0) {
+            // tmp is a divider of symbols. If we have constructed a symbol
+            // push it as the next token
+            tokens.push_back(symbol);
+            symbol = "";
+            if (tmp == '(' || tmp == ')') {
+                // If we are on a paren, push it too as the next token
+                std::string conv(1, tmp);
+                tokens.push_back(conv);
+            }
+        } else {
+            // Otherwise continue building the symbol
+            symbol.push_back(tmp);
+        }
+    }
+    
+    return tokens;
+}
--- tokenize.hpp
+++ tokenize.hpp
@@ -1,7 +1,15 @@
 #ifndef TOKENIZE_H
 #define TOKENIZE_H
 
+#include <vector>
+#include <istream>
+
+#include "expression.hpp"
+
 // This module should define the C++ types and code required to parse a
 // vtscript program into an AST.
+
+// Split the given stream into a set of tokens, stored as a vector of strings
+std::vector<std::string> tokenize(std::istream & in);
 
 #endif
