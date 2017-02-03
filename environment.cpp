01e4a774dc64f96ced351b14f8dc2b543af66c98
Nyle Rodgers <nyle.rodgers@gmail.com>
2017-02-02 22:06:31 -0500

    Initial implementation of environment

--- environment.cpp
+++ environment.cpp
@@ -1,1 +1,18 @@
 #include "environment.hpp"
+
+bool Environment::define(std::string symbol, Expression exp) {
+    if (!(this->map.count(symbol))) {
+        this->map[symbol] = exp;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+Expression Environment::retrieve(std::string symbol) {
+    if (this->map.count(symbol)) {
+        return this->map[symbol];
+    } else {
+        return Expression();
+    }
+}
--- environment.hpp
+++ environment.hpp
@@ -1,7 +1,22 @@
 #ifndef ENVIRONMENT_H
 #define ENVIRONMENT_H
 
+#include <map>
+#include <string>
+#include "expression.hpp"
+
 // This module should define the C++ types and code required to implement the
 // vtscript environment mapping.
+
+class Environment {
+private:
+    std::map<std::string, Expression> map;    
+public:
+    // Define a symbol. Returns true on success, false on failure
+    bool define(std::string symbol, Expression exp);
+    // Retrieve the given symbol. Returns None Expression if symbol is not
+    // defined
+    Expression retrieve(std::string symbol);
+};
 
 #endif // ENVIRONMENT_H
