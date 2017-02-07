55b5e118841cf4c819cd2a4e6822466d3763b0cf
Nyle Rodgers <nyle.rodgers@gmail.com>
2017-02-06 19:17:10 -0500

    reorganize vtscript.cpp

--- vtscript.cpp
+++ vtscript.cpp
@@ -3,22 +3,66 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <sstream>
 
 #include "tokenize.hpp"
 #include "expression.hpp"
 #include "environment.hpp"
 #include "interpreter.hpp"
-
+#include "interpreter_semantic_error.hpp"
 #include <functional>
 
+int repl();
+int run(std::istream & in);
+
 int main(int argc, char * argv[]) {
-    Interpreter interp = Interpreter();
-    std::ifstream in(argv[1]);
-    if (!interp.parse(in)) {
-        std::cout << "There was an error in parsing\n";
+    if (argc == 1) { // REPL
+        return repl();
+    } else if (argc == 3 && std::string(argv[1]) == "-e") { // from cmdline
+        std::stringstream lstream(argv[2]);
+        return run(lstream);
+    } else if (argc == 2 && std::string(argv[1]).substr(0, 2) == "-e") {
+        // from cmdline
+        std::stringstream lstream(std::string(argv[1]).substr(2));
+        return run(lstream);
+    } else if (argc == 2) {     // Run from a file
+        std::ifstream in(argv[1]);
+        if (!in.is_open()) {
+            std::cerr << "Unable to open the given file\n";
+            return EXIT_FAILURE;
+        }
+        return run(in);
+    } else {
+        std::cerr << "Too many inputs\n";
         return EXIT_FAILURE;
     }
-    std::cout << interp.eval() << "\n";
+}
 
+// get input from repl and evaluate untill eof
+int repl() {
+    Interpreter interp = Interpreter();
+    std::string line;
+    std::cout << "vtscript> ";
+    while (std::getline(std::cin, line)) {
+        std::stringstream lstream(line);
+        run(lstream);
+        std::cout << "vtscript> ";
+    }
+    return EXIT_SUCCESS;
+}
+
+// parse and run the given 
+int run(std::istream & in) {
+    Interpreter interp = Interpreter();
+    if (!interp.parse(in)) {
+        std::cerr << "There was an error in parsing\n";
+        return EXIT_FAILURE;
+    }
+    try {
+        std::cout << interp.eval() << "\n";
+    } catch (InterpreterSemanticError e) {
+        std::cerr << "A semantic error occured:\n";
+        std::cerr << e.what() << "\n";
+    }
     return EXIT_SUCCESS;
 }
