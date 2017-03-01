e4c011b95ae9f03db064184ed29c46d8a2f7c4e0
Nyle Rodgers <nyle.rodgers@gmail.com>
2017-03-01 12:06:50 -0500

    add more tests

--- test_tokenize.cpp
+++ test_tokenize.cpp
@@ -13,6 +13,15 @@ Expression s_to_ast(std::string prog) {
     return constructast(tokenlist);
 }
 
+TEST_CASE("Test parsing various valid inputs", "[tokenize]") {
+    REQUIRE_NOTHROW(s_to_ast("(5)"));
+    REQUIRE_NOTHROW(s_to_ast("(True)"));
+    REQUIRE_NOTHROW(s_to_ast("(define r 10)"));
+    REQUIRE_NOTHROW(s_to_ast("(* 5 2.6)"));
+    REQUIRE_NOTHROW(s_to_ast("(and True False)"));
+    REQUIRE_NOTHROW(s_to_ast("(begin (define r 10) (* pi (* r r)))"));
+}
+
 TEST_CASE("Test parsing program with comment", "[tokenize]") {
     REQUIRE_NOTHROW(s_to_ast("(define ; this is a comment\nr 10)"));
 }
